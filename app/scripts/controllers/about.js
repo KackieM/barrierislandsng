66c4174f2425d6e3fa16c5101afcfb16f7316546
Katherine Mattison <kbmedisto@gmail.com>
2014-06-25 19:02:50 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name barrierislandsngApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the barrierislandsngApp
+ */
+angular.module('barrierislandsngApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name barrierislandsngApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the barrierislandsngApp
+ */
+angular.module('barrierislandsngApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
